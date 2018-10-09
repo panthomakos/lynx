6944d1ce2b29b0a426d1f9999fc18cba569b1131
Sara Shi <sshi@strava.com>
2018-10-09 00:01:23 -0700

    Add lynx error and raise error upon stderr instead of aborting; allow higher level callers to handle error

--- lib/lynx/system_out.rb
+++ lib/lynx/system_out.rb
@@ -1,11 +1,12 @@
 require 'open3'
+require 'lynx/error'
 
 module Lynx
   module SystemOut
     def self.system_out_with_err(command)
       stdout, stderr, status = Open3.capture3(command)
       unless status.success?
-        abort("\n [ERROR] #{stderr} \n [ERROR] Failed to perform: #{command}")
+        raise(Lynx::Error, "\n [ERROR] #{stderr} \n [ERROR] Failed to perform: #{command}")
       end
     end
   end
